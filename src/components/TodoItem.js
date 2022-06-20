3572d3a6a2a74502592f3eb66e604e51474b544d
jaydto <johnckaris@gmail.com>
2022-06-20 22:45:50 +0300

    delete todo final impllementation

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ToggleButtonGroup } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
-import { deleteTodo, toggleComplete, toggleCompleteAsync } from '../redux/slice/TodoSlice';
+import { deleteTodo, deleteTodoAsync, toggleComplete, toggleCompleteAsync } from '../redux/slice/TodoSlice';
 
 const TodoItem = ({ id, title, completed }) => {
 	const dispatch=useDispatch();
 	const handleCompleteClick=()=>{dispatch(toggleCompleteAsync({id:id, completed:!completed}))}
-	const handleDeleteClick=()=>{dispatch(deleteTodo({id:id}))}
+	const handleDeleteClick=()=>{dispatch(deleteTodoAsync({id:id}))}
 	return (
 		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
 			<div className='d-flex justify-content-between'>
