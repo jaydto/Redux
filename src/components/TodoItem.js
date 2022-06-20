9e8ee680147f0b6933de464f55e07f4471d241a9
jaydto <johnckaris@gmail.com>
2022-06-20 21:45:43 +0300

    delete functionality done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ToggleButtonGroup } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
-import { toggleComplete } from '../redux/slice/TodoSlice';
+import { deleteTodo, toggleComplete } from '../redux/slice/TodoSlice';
 
 const TodoItem = ({ id, title, completed }) => {
 	const dispatch=useDispatch();
 	const handleCompleteClick=()=>{dispatch(toggleComplete({id:id, completed:!completed}))}
+	const handleDeleteClick=()=>{dispatch(deleteTodo({id:id}))}
 	return (
 		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
 			<div className='d-flex justify-content-between'>
@@ -13,7 +14,7 @@ const TodoItem = ({ id, title, completed }) => {
 					<input type='checkbox' className='mr-3' onChange={handleCompleteClick} checked={completed}></input>
 					{title}
 				</span>
-				<button className='btn btn-danger'>Delete</button>
+				<button onClick={handleDeleteClick} className='btn btn-danger'>Delete</button>
 			</div>
 		</li>
 	);
