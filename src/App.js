60cc37db6d0924b674b972be4901acea4ad5cda2
jaydto <johnckaris@gmail.com>
2022-06-20 22:55:18 +0300

    completed application fix Todo Route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom'
 import Home from './Routes/Home'
-import TodoList from './Routes/TodoList'
+import Info from './Routes/Info';
 
 const App = () => {
   return (
     <div className='container'>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/todo" element={<TodoList/>}/>
+        <Route path="/todo" element={<Info/>}/>
       </Routes>
     </div>
   )
--- src/Routes/Info.js
+++ src/Routes/Info.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import Navbar from '../components/Navbar'
 import TodoItem from '../components/TodoItem'
+import TodoList from '../components/TodoList'
 
-const TodoList = () => {
+const Info = () => {
     const todos=useSelector((state)=>state.todos)
   return (
     <div>
       <Navbar/>
-        <ul className="list-group">
-            {todos.map((todo)=>
-            <TodoItem id={todo.id} title={todo.title} completed={todo.complete}/>)}
-        </ul>
+    <TodoList/>
     </div>
   )
 }
 
-export default TodoList
+export default Info
